9fa6036ea20b40724d98e9c3def0059bc805c738
Min Thet Maung <minthetmaung@nexlabs.co>
2022-05-25 00:30:58 +0700

    split MenuItem code into separate file

--- UIDesign/Supporting Files/TriangleDrawableView.swift
+++ UIDesign/Supporting Files/TriangleDrawableView.swift
@@ -36,36 +36,3 @@ import UIKit
     }
 }
 
-
-
-@IBDesignable class MenuItem: UIView {
-    
-    @IBInspectable var borderWidth: CGFloat {
-        get { return layer.borderWidth }
-        set { layer.borderWidth = newValue }
-    }
-    
-    @IBInspectable var borderColor: UIColor {
-        get { return UIColor(cgColor: layer.borderColor ?? UIColor.clear.cgColor) }
-        set { layer.borderColor = newValue.cgColor }
-    }
-    
-    @IBInspectable var isTranslate: Bool {
-        get { return self.isTranslate }
-        set {
-            if newValue {
-                translate()
-            } else {
-                self.transform = .identity
-            }
-        }
-    }
-    
-    func translate() {
-        let angle: CGFloat = 25.0 * CGFloat.pi / 180.0
-        transform.c = sin(-angle)
-    }
-  
-}
-
-
